5ad3460ce98c60ed2e504c5c81d20dc7af10965f
Bartosz Piasecki <bpiasecki@iqsolution.pl>
2020-03-12 14:57:35 +0100

    tests fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('covid-app app is running!');
+    expect(compiled.querySelector('.app-title span').textContent).toContain('covid-app app is running!');
   });
 });

f821912a1b2b93c20632c0e4962d4a0e945a3f18
Bartosz Piasecki <bpiasecki@iqsolution.pl>
2020-03-13 21:49:38 +0100

    creating first page tiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid-app';
+  tiles: Tile[] = [
+    {text: 'COVID-19', cols: 6, rows: 1},
+    {text: 'Two', cols: 1, rows: 9},
+    {text: 'Three', cols: 3, rows: 9},
+    {text: 'Four', cols: 2, rows: 3},
+    {text: 'Five', cols: 2, rows: 3},
+    {text: 'Six', cols: 2, rows: 3},
+  ];
 }
